6cf614dd0a07aee3a1ed295a83155dd6422f5c1f
Irina Afanassenko <irina.afanassenko@gmail.com>
2022-11-08 09:20:46 +0100

    firebase connected project initialization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'auth-skillmeter',
+    appId: '1:935769185746:web:f1bc9a072b8ed97f2ffc0b',
+    storageBucket: 'auth-skillmeter.appspot.com',
+    apiKey: 'AIzaSyBnpNjk-b7AP1A4WM_nDuMkb26lK8wdhXM',
+    authDomain: 'auth-skillmeter.firebaseapp.com',
+    messagingSenderId: '935769185746',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'auth-skillmeter',
+    appId: '1:935769185746:web:f1bc9a072b8ed97f2ffc0b',
+    storageBucket: 'auth-skillmeter.appspot.com',
+    apiKey: 'AIzaSyBnpNjk-b7AP1A4WM_nDuMkb26lK8wdhXM',
+    authDomain: 'auth-skillmeter.firebaseapp.com',
+    messagingSenderId: '935769185746',
+  }
 };
 
 /*
