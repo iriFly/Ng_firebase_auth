d893c20d33f00797ec4e7fffc835367ffab62d24
Irina Afanassenko <irina.afanassenko@gmail.com>
2022-11-08 11:35:36 +0100

    forgot-password and varify-email pages were added

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -12,9 +12,16 @@ export class AuthService {
 
   //login method
   login(email: string, password: string) {
-    this.fireauth.signInWithEmailAndPassword(email, password).then ( () => {
+    this.fireauth.signInWithEmailAndPassword(email, password).then ( res => {
       localStorage.setItem('token', 'true');
+
+
+      if(res.user?.emailVerified == true) {
         this.router.navigate(['dashboard']);
+      } else {
+        this.router.navigate(['/varify-email'])
+      }
+
     }, err => {
       alert('Something went wrong. Please double check the credentials.');
       this.router.navigate(['/login']);
@@ -23,9 +30,10 @@ export class AuthService {
 
   //register method
   register(email:string, password: string ) {
-    this.fireauth.createUserWithEmailAndPassword(email, password).then( () => {
+    this.fireauth.createUserWithEmailAndPassword(email, password).then( res => {
       alert('Registration successful');
       this.router.navigate(['/login']);
+      this.sendEmailForVarification(res.user);
     }, err => {
       alert('Something went wrong. Please double check the credentials.');
       this.router.navigate(['/register']);
@@ -39,6 +47,25 @@ export class AuthService {
       this.router.navigate(['/login']);
     }, err => {
       alert('Something went wrong.');
+    })
+  }
+
+  //forgot password
+  forgotPassword(email: string) {
+    this.fireauth.sendPasswordResetEmail(email).then(() => {
+      this.router.navigate(['/varify-email']);
+    }, err => {
+      alert('Something went wrong.');
+    })
+  }
+
+  //email varification method
+  sendEmailForVarification(user: any){
+    user.sendEmailForVarification().then((res: any) => {
+      this.router.navigate(['/varify-email']);
+
+    }, (err : any) => {
+      alert('Something went wrong. Not able to reset password with the provided email.');
     })
   }
 
